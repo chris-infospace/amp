d270f8f5c8dc10c0afb592c793be09deca82fa26
Vikki Zarkovich <Vikki.Zarkovich@infospace.com>
2017-08-30 15:12:29 -0700

    data

---
+++ api.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+bingdata = {
+   "HelloWorld": "true"
+}
+
+class GetBingData(Resource):
+    def get(self):
+        return bingdata
+
+api.add_resource(GetBingData, '/Bing')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
